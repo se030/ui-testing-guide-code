3d7301943167767d733859971a4cf6ce1e532382
se030 <se030.kim@gmail.com>
2023-05-04 15:26:37 +0900

    feat: visual testing with stories and Chromatic

---
+++ src/components/Task.stories.js
@@ -0,0 +1,40 @@
+import { Task } from './Task';
+
+export default {
+  component: Task,
+  title: 'Task',
+  argTypes: {
+    onArchiveTask: { action: 'onArchiveTask' },
+    onTogglePinTask: { action: 'onTogglePinTask' },
+    onEditTitle: { action: 'onEditTitle' },
+  },
+};
+
+const Template = (args) => <Task {...args} />;
+
+export const Default = Template.bind({});
+Default.args = {
+  task: {
+    id: '1',
+    title: 'Buy milk',
+    state: 'TASK_INBOX',
+  },
+};
+
+export const Pinned = Template.bind({});
+Pinned.args = {
+  task: {
+    id: '2',
+    title: 'QA dropdown',
+    state: 'TASK_PINNED',
+  },
+};
+
+export const Archived = Template.bind({});
+Archived.args = {
+  task: {
+    id: '3',
+    title: 'Write schema for account menu',
+    state: 'TASK_ARCHIVED',
+  },
+};
